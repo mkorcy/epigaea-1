4d3f35217deee2e3047aa8123f1b84400a294ccb
Mike Korcynski <mkorcy@gmail.com>
2018-03-13 10:41:17 -0400

    adds specs for year facet

--- spec/support/shared_examples/metadata_attributes.rb
+++ spec/support/shared_examples/metadata_attributes.rb
@@ -2,6 +2,7 @@ shared_examples 'a work with Tufts metadata attributes' do
   it_behaves_like 'and has admin metadata attributes'
   it_behaves_like 'a work with custom Tufts validations'
   it_behaves_like 'a record with ordered fields'
+  it_behaves_like 'a work with facetable years'
 
   context 'and the list of metadata attributes' do
     it 'has displays_in' do
