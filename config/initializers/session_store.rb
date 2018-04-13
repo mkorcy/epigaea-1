39e3806d09918fc92ac67da36bb1a500f262a2d0
Mike Korcynski <mkorcy@gmail.com>
2018-04-13 12:18:26 -0400

    moves sessions to redis

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,18 @@
 # Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :redis_store, key: '_epigea_session'
+if Rails.env.production?
+Rails.application.config.session_store :redis_store, {
+  servers: [
+    {
+      host: "localhost",
+      port: 6379,
+      db: 0,
+      password: Rails.application.config_for(:redis)["password"],
+      namespace: "session"
+    },
+  ],
+  expire_after: 90.minutes,
+  key: "_epigaea_session"
+}
+else
+Rails.application.config.session_store :redis_store
+end
