cde90e7ae2db20c6a86bd464be48228d51aadd76
Travis Lilleberg <travis.lilleberg@tufts.edu>
2018-05-25 12:18:47 -0400

    Alphabatized the collections in the list

--- app/helpers/tufts/tufts_helper_behavior.rb
+++ app/helpers/tufts/tufts_helper_behavior.rb
@@ -10,5 +10,13 @@ module Tufts
                                         presenter_class: "Hyrax::#{model.class}Presenter".classify.safe_constantize,
                                         presenter_args: nil)[0].solr_document["workflow_state_name_ssim"]
     end
+
+    # Gets all this user's collections, sorted alphabetically.
+    # In use in _form_for_select_collection.html.erb
+    def alpha_user_collections
+      @user_collections.sort do |c1, c2|
+        c1[:title_tesim].first.downcase <=> c2[:title_tesim].first.downcase
+      end
+    end
   end
 end
