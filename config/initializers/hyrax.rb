092479209d24ef00dd0f0a53cc432737e5241fd6
Travis Lilleberg <travis.lilleberg@tufts.edu>
2018-11-06 11:28:16 -0500

    Added analytic_start_date to initializer

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -44,6 +44,7 @@ Hyrax.config do |config|
   if Rails.env == 'production'
     config.analytics = true
     config.google_analytics_id = 'UA-38431398-6'
+    config.analytic_start_date = Time.zone.local(2018, 11, 1)
   end
 
   # Date you wish to start collecting Google Analytic statistics for
