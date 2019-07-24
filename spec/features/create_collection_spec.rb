a7f621ea75872d7138bad679d1bef3a7ffcf20f6
Mike Korcynski <mkorcy@gmail.com>
2019-07-24 11:41:39 -0400

    account for changes in collections ui

--- spec/features/create_collection_spec.rb
+++ spec/features/create_collection_spec.rb
@@ -2,6 +2,7 @@
 #  `rails generate hyrax:work Ead`
 require 'rails_helper'
 require 'ffaker'
+require 'byebug'
 include Warden::Test::Helpers
 
 # NOTE: If you generated more than one work, you have to set "js: true"
@@ -10,18 +11,21 @@ RSpec.feature 'Create a Collection', :clean, js: true do
     let(:user) { FactoryGirl.create(:admin) }
     let(:title) { FFaker::Book.title }
     let(:ead_id) { 'fake_ead_id' }
+    let!(:user_collection_type) { create(:user_collection_type) }
 
     before { login_as user }
 
     scenario do
       visit '/dashboard'
       click_link "Collections"
+      sleep(2)
       click_link "New Collection"
 
       # EAD is a form entry field when you create a new Collection
       fill_in 'Title', with: title
       fill_in 'EAD', with: ead_id
-      click_button "Create Collection"
+      click_button "Save"
+      click_link "Cancel"
 
       # EAD id is displayed on the Collection page within the admin dashboard
       expect(page).to have_content "EAD"
