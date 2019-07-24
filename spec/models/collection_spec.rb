f2d79a719c233f0c132d6fc2f69fc563bfa5ce05
Mike Korcynski <mkorcy@gmail.com>
2019-07-24 09:28:38 -0400

    use collection factory

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Collection, type: :model do
-  subject(:collection) { FactoryGirl.build(:collection, ead: ead_id) }
-  let(:ead_id) { ["ead123"] }
+  subject(:collection) { FactoryGirl.build(:collection_lw) }
 
   it_behaves_like 'a record with ordered fields' do
     let(:work) { collection }
   end
 
   it "can have an associated EAD" do
-    expect(collection.ead).to eq ead_id
+    expect(collection.ead.first).to start_with "EAD id"
   end
 end
