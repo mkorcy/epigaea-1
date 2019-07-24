0662aa97f164ee2d5995d84227e158f55929af28
Mike Korcynski <mkorcy@gmail.com>
2019-07-23 23:42:49 -0400

    removes AF::Noid use Noid::Rails where needed

--- spec/factories/image.rb
+++ spec/factories/image.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do
   factory :image do
-    id { ActiveFedora::Noid::Service.new.mint }
     title ["Image: #{FFaker::Movie.title}"]
     visibility Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC
 
--- spec/factories/rcr.rb
+++ spec/factories/rcr.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do
   factory :rcr do
-    id { ActiveFedora::Noid::Service.new.mint }
     title ['Test']
     visibility Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC
   end
