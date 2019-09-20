30a21ea5cbf7fe8df27f683ea60b89eb23a63748
Mike Korcynski <mkorcy@gmail.com>
2019-09-20 11:18:48 -0400

    add creator to image factory

--- spec/factories/image.rb
+++ spec/factories/image.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :image do
     title ["Image: #{FFaker::Movie.title}"]
+    creator ["Image Creator"]
     visibility Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC
 
     transient do
