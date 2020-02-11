3f6bb05a665c13366bf7a72ec99c0c03685da203
Mike Korcynski <mkorcy@gmail.com>
2020-02-11 16:05:35 -0500

    adds a back to app button to header

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 require 'sidekiq-limit_fetch'
-
+require 'sidekiq/web'
 auth = if Rails.application.config_for(:redis)["password"]
          ":" + Rails.application.config_for(:redis)["password"] + "@"
        else
@@ -13,3 +13,5 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: 'redis://' + auth + 'localhost:6379' }
 end
+
+Sidekiq::Web.app_url = '/dashboard'
