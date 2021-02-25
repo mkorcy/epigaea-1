9e7c4c81bc8f9f046b784a34234f82d4f2f56dd3
Mike Korcynski <mkorcy@gmail.com>
2021-02-25 17:28:40 -0500

    makes the latency checks on the queues optional to return 200

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -1,20 +1,5 @@
 require 'net/smtp'
 
-# Check to ensure we can connect to SMTP as expected. We are writing our own because the
-# ActionMailer test bundled with okcomputer doesn't seem to work for Amazon SES.
-class SmtpCheck < OkComputer::Check
-  def check
-    smtp = Net::SMTP.new ENV['ACTION_MAILER_SMTP_ADDRESS'], ENV['ACTION_MAILER_PORT']
-    smtp.enable_starttls
-    smtp.start('curationexperts.com', ENV['ACTION_MAILER_USER_NAME'], ENV['ACTION_MAILER_PASSWORD'], :plain) do
-      mark_message "SMTP connection working"
-    end
-  rescue
-    mark_failure
-    mark_message "Cannot connect to SMTP"
-  end
-end
-
 # Monitor sidekiq queue latency. Queue latency is the difference between when the
 # oldest job was pushed onto the queue versus the current time. This code will
 # check that jobs don't spend more than 360 seconds enqueued. We'll need to adjust
@@ -25,4 +10,4 @@ OkComputer::Registry.register "batch_sidekiq_queue_latency", OkComputer::Sidekiq
 OkComputer::Registry.register "handle_sidekiq_queue_latency", OkComputer::SidekiqLatencyCheck.new('handle', 360)
 OkComputer::Registry.register "default_sidekiq_queue_latency", OkComputer::SidekiqLatencyCheck.new('default', 360)
 
-OkComputer::Registry.register "smtp", SmtpCheck.new
+OkComputer.make_optional %w[handle_sidekiq_queue_latency batch_sidekiq_queue_latency handle_sidekiq_queue_latency default_sidekiq_queue_latency]
