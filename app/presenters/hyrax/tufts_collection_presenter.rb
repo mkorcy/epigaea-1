4c7f89649a4f2602a347f7f975476171e30f5665
Mike Korcynski <mkorcy@gmail.com>
2021-03-01 19:08:29 -0500

    rubocop cleanup

--- app/presenters/hyrax/tufts_collection_presenter.rb
+++ app/presenters/hyrax/tufts_collection_presenter.rb
@@ -6,26 +6,28 @@ module Hyrax
     def nested_collection_pathnames
       solr_document['nesting_collection__pathnames_ssim']
     end
+
+    # rubocop:disable Metrics/MethodLength
     def nested_collection_names_and_links
       cols = nested_collection_pathnames
       list_of_lists = []
-      enhanced_id_list = Array.new
+      enhanced_id_list = []
       cols.each do |col|
         parsed_ids = col.split('/')
         parsed_ids.delete(id)
-        enhanced_id_list = Array.new
+        enhanced_id_list = []
         parsed_ids.each do |p_id|
           results = ActiveFedora::SolrService.query(ActiveFedora::SolrQueryBuilder.construct_query(id: p_id), rows: 1, fl: 'title_tesim')
           title_tesim = results[0]['title_tesim']
-          enhanced_id_list.push({id: p_id, title: title_tesim[0]})
+          enhanced_id_list.push(id: p_id, title: title_tesim[0])
         end
         list_of_lists << enhanced_id_list
       end
       list_of_lists
     end
+
     def self.terms
       Hyrax::CollectionPresenter.terms + [:ead]
     end
-
   end
 end
